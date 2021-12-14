4294d1f7def1102b2e3198b8bf97faed4bd9f141
Dirga <dirgantoro.developer@gmail.com>
2021-12-14 19:01:24 +0700

    test

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -32,18 +32,18 @@ export default function TodoList() {
       <div className="container">        
         
         <div className="notification is-warning small">
-          {/* <strong><span role="img" aria-label="UndianBerhadiah">🎁</span></strong><br/> */}
+          <strong><span role="img" aria-label="UndianBerhadiah">🎁</span></strong><br/>
           
           <DonutChart value={50} greeting={total} />
           <br/>
-          <strong>Jl Cucut Bangil no 175 <a href='https://www.google.co.id/maps/place/Jl.+Cucut+No.175,+Sukomangu,+Bendo+Mungal,+Kec.+Bangil,+Pasuruan,+Jawa+Timur+67153/@-7.5921305,112.7821313,17z/data=!4m13!1m7!3m6!1s0x2dd7db60d0f09c9f:0xdba160d74c0a1ef6!2sJl.+Cucut+No.175,+Sukomangu,+Bendo+Mungal,+Kec.+Bangil,+Pasuruan,+Jawa+Timur+67153!3b1!8m2!3d-7.5921305!4d112.78432!3m4!1s0x2dd7db60d0f09c9f:0xdba160d74c0a1ef6!8m2!3d-7.5921305!4d112.78432'><span role="img" aria-label="dollar">🗺️</span></a></strong>
+          <strong>Jl Cucut Bangil no 175 <a href='https://www.google.co.id/maps/place/Jl.+Cucut+No.175,+Sukomangu,+Bendo+Mungal,+Kec.+Bangil,+Pasuruan,+Jawa+Timur+67153/@-7.5921305,112.7821313,17z/data=!4m13!1m7!3m6!1s0x2dd7db60d0f09c9f:0xdba160d74c0a1ef6!2sJl.+Cucut+No.175,+Sukomangu,+Bendo+Mungal,+Kec.+Bangil,+Pasuruan,+Jawa+Timur+67153!3b1!8m2!3d-7.5921305!4d112.78432!3m4!1s0x2dd7db60d0f09c9f:0xdba160d74c0a1ef6!8m2!3d-7.5921305!4d112.78432'><span  role="img" aria-label="dollar" >🗺️</span></a></strong>
           
         </div>
 
         <table className="table is-fullwidth">
           <thead>
             <tr>
-              <th><span role="img" aria-label="dollar">🎫</span></th>
+              <th><span role="img" aria-label="dollar" >🎫</span></th>
               <th>Identitas</th>
               <th>Ayam</th>
             </tr>
